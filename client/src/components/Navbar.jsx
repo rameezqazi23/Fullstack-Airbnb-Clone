78aac6d938beb0e3e9ae55c82e193b7c6ec5d8c5
rameezqazi23 <rameez.qazi23@gmail.com>
2023-10-25 22:06:36 +0500

    Create Signup page and add some minor changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,58 +8,58 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     const [toggle, setToggle] = useState(false);
 
+
     return (
-        <nav className='w-full flex justify-between px-8 py-4 mx-auto border-b border-gray-300'>
-            <a href="/" className='flex items-center gap-1'>
-                <TbBrandAirbnb color='#ff5a60' size={35} />
-                <span className='text-[24px] font-bold text-primary'>airbnb</span>
-            </a>
-            <div className='sm:flex hidden items-center rounded-full gap-3 px-6 py-2 bg-[#f5f5f5] shadow-md border border-gray-300'>
-                <p className='cursor-pointer font-semibold text-gray-500 hover:text-black'>Anywhere</p>
-                <d className="border border-l border-gray-300 h-[18px]"></d>
-                <p className='cursor-pointer font-semibold text-gray-500 hover:text-black'>Anyweek</p>
-                <div className="border border-l border-gray-300 h-[18px]"></div>
-                <p className='cursor-pointer font-semibold text-gray-500 hover:text-black'>Add guest</p>
-                <button className='bg-primary rounded-full p-2'>
-                    <FiSearch color='white' />
-                </button>
-            </div>
-            <div className='flex items-center justify-between rounded-full gap-3 px-6 py-2 bg-[#f5f5f5] border border-gray-300'>
+        <>
+            <nav className='w-full flex justify-between px-8 py-4 mx-auto border-b border-gray-300'>
+                <a href="/" className='flex items-center gap-1'>
+                    <TbBrandAirbnb color='#ff5a60' size={35} />
+                    <span className='text-[24px] font-bold text-primary'>airbnb</span>
+                </a>
+                <div className='sm:flex hidden items-center rounded-full gap-3 px-6 py-2 bg-[#f5f5f5] shadow-md border border-gray-300'>
+                    <p className='cursor-pointer font-semibold text-gray-500 hover:text-black'>Anywhere</p>
+                    <d className="border border-l border-gray-300 h-[18px]"></d>
+                    <p className='cursor-pointer font-semibold text-gray-500 hover:text-black'>Anyweek</p>
+                    <div className="border border-l border-gray-300 h-[18px]"></div>
+                    <p className='cursor-pointer font-semibold text-gray-500 hover:text-black'>Add guest</p>
+                    <button className='bg-primary rounded-full p-2'>
+                        <FiSearch color='white' />
+                    </button>
+                </div>
+                <div className='flex items-center justify-between rounded-full gap-3 px-6 py-2 bg-[#f5f5f5] border border-gray-300'>
 
-                <HiMiniBars3BottomRight className='sm:hidden flex cursor-pointer' onClick={() => setToggle(!toggle)} />
+                    <HiMiniBars3BottomRight className='sm:hidden flex cursor-pointer' onClick={() => setToggle(!toggle)} />
 
-                <div className={`${!toggle ? "hidden" : "flex"}
+                    <div className={`${!toggle ? "hidden" : "flex"}
                  p-6 text-white absolute top-20 right-0 mx-4 my-2 min-w-[180px] z-10 rounded-xl
                  backdrop-filter backdrop-blur-lg bg-opacity-70 bg-[#f5f5f5] shadow-md`}>
 
-                    <ul className='list-none flex flex-col gap-4 justify-end items-start'>
-                        <li className='text-gray-500 hover:text-black text-[16px] font-medium cursor-pointer'>Anywhere</li>
-                        <li className='text-gray-500 hover:text-black text-[16px] font-medium cursor-pointer'>Anyweek</li>
-                        <li className='text-gray-500 hover:text-black text-[16px] font-medium cursor-pointer'>Addguest</li>
+                        <ul className='list-none flex flex-col gap-4 justify-end items-start'>
+                            <li className='text-gray-500 hover:text-black text-[16px] font-medium cursor-pointer'>Anywhere</li>
+                            <li className='text-gray-500 hover:text-black text-[16px] font-medium cursor-pointer'>Anyweek</li>
+                            <li className='text-gray-500 hover:text-black text-[16px] font-medium cursor-pointer'>Addguest</li>
 
-                    </ul>
-                </div>
+                        </ul>
+                    </div>
 
 
-                <FaUserCircle className='cursor-pointer opacity-50' onClick={() => setToggle(!toggle)} size={28} />
+                    <FaUserCircle className='cursor-pointer opacity-50' onClick={() => setToggle(!toggle)} size={28} />
 
-                <div className={`${!toggle ? "hidden" : "flex"}
+                    <div className={`${!toggle ? "hidden" : "flex"}
                  p-6 text-white absolute top-20 right-0 mx-4 my-2 min-w-[180px] z-10 rounded-xl
                  backdrop-filter backdrop-blur-lg bg-opacity-80 bg-[#e7e6e6] shadow-md`}>
 
-                    <div className='list-none flex flex-col gap-4 justify-end items-start'>
-                        <Link to='/signup' className='text-gray-500 hover:text-black text-[16px] font-medium cursor-pointer'>Sign up</Link>
-                        <Link to='/signin' className='text-gray-500 hover:text-black text-[16px] font-medium cursor-pointer'>Login</Link>
-                        <Link to='/profile' className='text-gray-500 hover:text-black text-[16px] font-medium cursor-pointer'>Profile</Link>
+                        <div className='list-none flex flex-col gap-4 justify-end items-start'>
+                            <Link to='/signup' className='text-gray-500 hover:text-black text-[16px] font-medium cursor-pointer'>Sign up</Link>
+                            <Link to='/signin' className='text-gray-500 hover:text-black text-[16px] font-medium cursor-pointer'>Login</Link>
+                            <Link to='/profile' className='text-gray-500 hover:text-black text-[16px] font-medium cursor-pointer'>Profile</Link>
 
+                        </div>
                     </div>
+
                 </div>
-
-            </div>
-
-
-
-        </nav>
+            </nav>
+        </>
     )
 }
 
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,33 @@
+import { Link } from "react-router-dom"
 
 const SignUp = () => {
+
   return (
-    <div>
-        SignUp
-      
+    <div className="w-full h-full">
+      <div className="fixed w-full px-4 py-16 z-50">
+        <div className="max-w-[500px] h-[600px] mx-auto overflow-visible ">
+          <div className="max-w-[380px] mx-auto py-12 px-7 rounded-md border border-gray-300 ">
+            <h1 className="text-2xl font-bold text-gray-700">Create Account</h1>
+
+            <form className="w-full flex flex-col py-4">
+              <input type="text" autoComplete="on" placeholder="John" className='p-2 my-2 border border-gray-400 outline-none rounded text-gray-700' required autoFocus/>
+              <input type="email" autoComplete="on" placeholder="youremail@xyz.com" className='p-2 my-2 border border-gray-400 outline-none rounded text-gray-700' required />
+              <input type="password" placeholder="password" className='p-2 my-2 border border-gray-400 outline-none rounded text-gray-700' required/>
+              <button className="bg-primary text-white font-medium text-[18px] p-2 rounded">Sign up</button>
+            <div className="flex gap-2 mt-4">
+              <p className="text-[16px] text-gray-600">Already have an account</p>
+              <Link to='/signin' className="text-[16px] underline font-semibold">SignIn</Link>
+            </div>
+            </form>
+
+          </div>
+
+        </div>
+
+      </div>
+
+
+
     </div>
   )
 }

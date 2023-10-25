0753d1a505d92e56540413e032a994919f4dfb52
rameezqazi23 <rameez.qazi23@gmail.com>
2023-10-25 16:30:27 +0500

    Add react router for navigating pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
 import { Navbar } from "./components"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home, Profile, SignIn, SignUp } from "./pages";
 
 function App() {
 
   return (
     <>
-      <Navbar />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/profile" element={<Profile />} />
+
+        </Routes>
+      </Router>
+
     </>
   )
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { TbBrandAirbnb } from "react-icons/tb";
 import { FiSearch } from "react-icons/fi";
 import { FaUserCircle } from "react-icons/fa";
 import { HiMiniBars3BottomRight } from "react-icons/hi2"
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     const [toggle, setToggle] = useState(false);
-    
+
     return (
         <nav className='w-full flex justify-between px-8 py-4 mx-auto border-b border-gray-300'>
             <a href="/" className='flex items-center gap-1'>
@@ -24,32 +25,34 @@ const Navbar = () => {
                 </button>
             </div>
             <div className='flex items-center justify-between rounded-full gap-3 px-6 py-2 bg-[#f5f5f5] border border-gray-300'>
+
                 <HiMiniBars3BottomRight className='sm:hidden flex cursor-pointer' onClick={() => setToggle(!toggle)} />
 
                 <div className={`${!toggle ? "hidden" : "flex"}
-                 p-6 text-white absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl
+                 p-6 text-white absolute top-20 right-0 mx-4 my-2 min-w-[180px] z-10 rounded-xl
                  backdrop-filter backdrop-blur-lg bg-opacity-70 bg-[#f5f5f5] shadow-md`}>
 
                     <ul className='list-none flex flex-col gap-4 justify-end items-start'>
-                        <li className='text-gray-600 hover:text-black text-[16px] font-medium cursor-pointer'>Anywhere</li>
-                        <li className='text-gray-600 hover:text-black text-[16px] font-medium cursor-pointer'>Anyweek</li>
-                        <li className='text-gray-600 hover:text-black text-[16px] font-medium cursor-pointer'>Addguest</li>
+                        <li className='text-gray-500 hover:text-black text-[16px] font-medium cursor-pointer'>Anywhere</li>
+                        <li className='text-gray-500 hover:text-black text-[16px] font-medium cursor-pointer'>Anyweek</li>
+                        <li className='text-gray-500 hover:text-black text-[16px] font-medium cursor-pointer'>Addguest</li>
 
                     </ul>
                 </div>
 
-                <FaUserCircle className='cursor-pointer opacity-50' onClick={()=>setToggle(!toggle)} size={28} />
+
+                <FaUserCircle className='cursor-pointer opacity-50' onClick={() => setToggle(!toggle)} size={28} />
 
                 <div className={`${!toggle ? "hidden" : "flex"}
-                 p-6 text-white absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl
+                 p-6 text-white absolute top-20 right-0 mx-4 my-2 min-w-[180px] z-10 rounded-xl
                  backdrop-filter backdrop-blur-lg bg-opacity-80 bg-[#e7e6e6] shadow-md`}>
 
-                    <ul className='list-none flex flex-col gap-4 justify-end items-start'>
-                        <li className='text-gray-600 hover:text-black text-[16px] font-medium cursor-pointer'>Sign up</li>
-                        <li className='text-gray-600 hover:text-black text-[16px] font-medium cursor-pointer'>Login</li>
-                        <li className='text-gray-600 hover:text-black text-[16px] font-medium cursor-pointer'>Profile</li>
+                    <div className='list-none flex flex-col gap-4 justify-end items-start'>
+                        <Link to='/signup' className='text-gray-500 hover:text-black text-[16px] font-medium cursor-pointer'>Sign up</Link>
+                        <Link to='/signin' className='text-gray-500 hover:text-black text-[16px] font-medium cursor-pointer'>Login</Link>
+                        <Link to='/profile' className='text-gray-500 hover:text-black text-[16px] font-medium cursor-pointer'>Profile</Link>
 
-                    </ul>
+                    </div>
                 </div>
 
             </div>

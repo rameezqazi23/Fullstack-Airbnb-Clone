433d3be1373c2e8fb21271cfc7b31b6165c2daf2
rameezqazi23 <rameez.qazi23@gmail.com>
2023-10-26 20:47:26 +0500

    Create password hashing for security

--- api/models/user.js
+++ api/models/user.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { createHmac, randomBytes } from "crypto";
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -11,11 +12,17 @@ const UserSchema = new mongoose.Schema({
         required: true,
         unique: true
     },
+    salt: {
+        type: String,
+    },
     password: {
         type: String,
         required: true,
     },
-
+    verified: {
+        type: Boolean,
+        default: false,
+    },
     profileImageUrl: {
         type: String,
         default: '/public/images/avatar.png'
@@ -23,6 +30,23 @@ const UserSchema = new mongoose.Schema({
 
 
 }, { timestamps: true })
+
+UserSchema.pre("save", function (next) {
+    const user = this;
+    if (!user.isModified("password")) return
+
+    const salt = randomBytes(16).toString(); //this actually generate a random key
+
+    const hashedPassword = createHmac('sha256', salt)
+        .update(user.password)
+        .digest('hex')
+
+    this.salt = salt;
+    this.password = hashedPassword;
+
+    next();
+
+})
 
 const USER = mongoose.model('users', UserSchema)
 

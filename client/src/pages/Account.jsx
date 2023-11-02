334d578164877c6217720d25a7fcac3f9e637f17
rameezqazi23 <rameez.qazi23@gmail.com>
2023-11-02 16:01:18 +0500

    Issue resolved for same page navigation nested routing.

--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -5,7 +5,7 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 const Account = () => {
     const { user, isUserAvailable } = useContext(UserContext);
     const navigate = useNavigate();
-    let { subpage } = useParams();
+    const { subpage } = useParams();
     console.log("Subpage",subpage)
 
     if (!isUserAvailable) {
@@ -21,8 +21,8 @@ const Account = () => {
         <div>
             <nav className="w-full flex justify-center mt-8 gap-10">
                 <Link to={'/account'} className="py-2 px-6 text-white bg-primary rounded-full text-[16px] font-medium cursor-pointer">Profile</Link>
-                <Link to={'account/places'} className="py-2 px-6 text-[16px] font-medium cursor-pointer text-gray-500 hover:text-black">Accommodations</Link>
-                <Link to={'acount/bookings'} className="py-2 px-6 text-[16px] font-medium cursor-pointer text-gray-500 hover:text-black">Booking</Link>
+                <Link to={'/account/places'} className="py-2 px-6 text-[16px] font-medium cursor-pointer text-gray-500 hover:text-black">Accommodations</Link>
+                <Link to={'/account/bookings'} className="py-2 px-6 text-[16px] font-medium cursor-pointer text-gray-500 hover:text-black">Booking</Link>
             </nav>
         </div>
     )

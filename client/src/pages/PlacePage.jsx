8e3183317c57cdd2b40ee858548b6fcbf48093c1
rameezqazi23 <rameez.qazi23@gmail.com>
2023-11-19 19:02:14 +0500

    Add some changes

--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -2,16 +2,15 @@ import { useContext } from 'react'
 import { PlaceContext } from '../context/placeContext'
 import { useParams } from 'react-router-dom';
 
-
-
 const PlacePage = () => {
-    const { place } = useContext(PlaceContext);
+    const { places } = useContext(PlaceContext);
+    const result = places.map((place) => (place._id))
     const { id } = useParams();
-    console.log("Params id", id)
+    console.log("Params id", places)
     return (
         <div>
-            This is full Place Page
-            {id}
+            This is full Place Page <br />
+            {id === result && result}
 
         </div>
     )
